8a374d6e1618c9ca0d274e0727d6a64201ee73ac
Witold Radzik <witoldradzik@student.agh.edu.pl>
2024-11-03 16:24:47 +0100

    bazowa strona

--- Projekt01/various.js
+++ Projekt01/various.js
@@ -1,3 +1,4 @@
+
 window.onload = function(){
     document.addEventListener("keydown", (event) => {
     if (event.keyCode === 82) {
@@ -16,4 +17,31 @@ window.onload = function(){
         console.log("is pink");
     }
 });
+    var obj;
+try{
+fetch('http://api.weatherapi.com/v1/current.json?key=b8c82a0927d14f588da205229240211&q=Leipzig')
+  .then(res => res.json())
+  .then(data => {
+    obj = data;
+   })
+  .then(() => {
+    console.log(obj.current.condition.text);
+    console.log(obj.current.condition.icon);
+    console.log(obj.current.temp_c);
+    console.log(obj.current.wind_kph);
+    document.getElementById("weather").innerHTML = `
+    <h3>Fun fact! This is the current weather in Hanna's hometown:</h3>
+    ${obj.current.condition.text}<br/>
+    Temperature: ${obj.current.temp_c}°c<br/>
+    Wind: ${obj.current.wind_kph} km/h<br/>
+    Wind direction: ${obj.current.wind_dir}
+
+    
+    `;
+   });
+}
+catch{
+    document.getElementById("weather").innerHTML =`
+    This was supposed to be a weather report from her hometown, but API seems to be down. Instead, fun fact: she has a cat which is very cute.`
+}
 }
